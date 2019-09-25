612de5f95ff33a4e85937850e7c09819b1e7d8ac
Victor L. Senior <publicdomainvic@gmail.com>
2019-09-25 07:15:52 -0400

    Add OnExitToolStripMenuItemClick() code

--- TitleChangerLauncherGenerator/MainForm.cs
+++ TitleChangerLauncherGenerator/MainForm.cs
@@ -48,7 +48,8 @@ namespace TitleChangerLauncherGenerator
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code.
+            // Close program
+            this.Close();
         }
 
         /// <summary>
